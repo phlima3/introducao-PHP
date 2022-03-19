61592a2fdee8fe640e36b01b3d78f180ff1418c7
Pedro Lima <psantoslima86@gmail.com>
2022-03-18 22:43:58 -0300

    pedro
    aula

---
+++ php_aula/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<body>
+    <?php
+  
+    echo "String <br>";
+$array_times = array("São Paulo", "Corinthians" , "Palmeiras");
+var_dump($array_times);
+echo "<br> <br>";
+    echo "Inteiro <br>";
+$array_inteiro = array("36", "49" , "101");
+var_dump(($array_inteiro));
+echo "<br> <br>";
+    echo "Decimal <br>";
+$array_decimal = array("1.14", "3.37" , "10.93");
+var_dump($array_decimal);
+
+
+
+    ?>
+</body>
+
+</html>
